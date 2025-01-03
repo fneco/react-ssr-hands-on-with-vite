9cf4f1b04321cf1f625ef38ce27c1e87f2cb8b46
fneco <izumi.f.tech@gmail.com>
2025-01-03 16:17:00 +0900

    use renderToString simply

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { createElement } from "react";
+import ReactDOMServer from "react-dom/server";
+import { defineConfig, PluginOption } from "vite";
+import App from "./src/App.tsx";
+
+const htmlPlugin = (): PluginOption => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      const appHtml = ReactDOMServer.renderToString(createElement(App));
+      return html.replace(/%App%/, appHtml);
+    },
+  };
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPlugin()],
 })
