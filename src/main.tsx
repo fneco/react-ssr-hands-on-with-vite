275adf9ede7bec754a0a8e1553d723365d8ef470
fneco <izumi.f.tech@gmail.com>
2025-01-05 03:09:34 +0900

    createRootでも動く

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
-import { hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-hydrateRoot(document.getElementById('root')!,
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
